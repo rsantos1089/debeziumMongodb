79b595a6bdfc5533b0c8067475f70327dd83a976
rsantos1089@gmail.com <rsantos1089@gmail.com>
2022-05-24 14:02:33 +0000

    update view query

--- bigquery/view/myMovies_view.sql
+++ bigquery/view/myMovies_view.sql
@@ -1,28 +1,24 @@
 CREATE OR REPLACE VIEW analytics_video.myMovies_view AS
 
 WITH myMovies_delta AS (
-  SELECT concat("[",after,"]") content_after ,
-  patch,filter,updateDescription,source,op,(case when op = 'd' then ts_ms -1 else ts_ms end ) ts_ms,transaction
+ 
+ SELECT STRUCT(JSON_EXTRACT(after,"$._id") AS _id,
+               JSON_EXTRACT(after,"$.title") AS title, 
+  	           JSON_EXTRACT(after,"$.year") AS year, 
+  	           JSON_EXTRACT(after,"$.type") AS type ) AS after ,
+  patch, STRUCT(JSON_EXTRACT(filter,"$._id")  AS _id) AS filter,updateDescription,
+  source,op,(case when op = 'd' then ts_ms -1 else ts_ms end ) ts_ms,transaction
   from `chrome-coast-348406.analytics_video.myMovies_delta`
+
 ),
 
-myMovies_arrange AS(
-SELECT 
-array( 
-  SELECT AS struct
-  STRUCT(JSON_EXTRACT(content_after,"$._id") AS _id, JSON_EXTRACT(content_after,"$.title") AS title, JSON_EXTRACT(content_after,"$.year") AS year, JSON_EXTRACT(content_after,"$.type") AS type ) AS payload
-  from unnest(analytics_video.json2array(content_after)) AS content_after
-) as after,
-patch,filter,updateDescription	,source,op,ts_ms,transaction
-FROM myMovies_delta
- ),
-
-myMovies_arrange_final as(
- select payload as after,patch,filter,updateDescription	,source,op,ts_ms,transaction
-  FROM myMovies_arrange r  CROSS JOIN UNNEST(r.after) as after
+generate_pk as(
+ select * , (CASE WHEN after._id is null then filter._id else after._id end) as PK_ID 
+ from  myMovies_delta
 )
 
-SELECT * EXCEPT(op, row_num) FROM (
- SELECT *, ROW_NUMBER() OVER (PARTITION BY after._id ORDER BY ts_ms DESC) AS row_num
-from myMovies_arrange_final )
+SELECT * EXCEPT(op, row_num,PK_ID) FROM (
+select *,ROW_NUMBER() OVER (PARTITION BY PK_ID ORDER BY ts_ms DESC) AS row_num
+from generate_pk )
 WHERE row_num = 1 	AND op <> 'd' ;
+
